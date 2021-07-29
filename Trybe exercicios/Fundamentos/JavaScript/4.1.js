99d74cec2f1842e6e0460da236749e60a793dffa
PedroAugmed <87546291+PedroAugmed@users.noreply.github.com>
2021-07-29 16:01:47 -0300

    Add files via upload
    Start Repository

---
+++ Trybe exercicios/Fundamentos/JavaScript/4.1.js
@@ -0,0 +1,150 @@
+/*EXERCICIO 1
+  
+Selocione o Codigo e clique no Play o Resultado aparecera no seu Output
+Calculadora de Adição*/                                                                
+let a = 7;
+let b = 7;
+let sum = a + b;
+
+console.log(sum)
+    
+// Calculadora de Subtração
+let a = 7;
+let b = 7;
+let sum = a - b;
+
+console.log(sum)
+
+// Calculadora de Multiplicação
+let a = 7;
+let b = 7;
+let sum = a * b;
+
+console.log(sum)
+
+// Calculadora de Divisão
+let a = 7;
+let b = -7;
+let sum = a / b;
+
+console.log(sum)
+
+// Calculadora de Resto de Divisão
+let a = 7;
+let b = 7;
+let sum = a % b;
+
+console.log(sum)
+       
+
+/* EXERCICIO 2 
+Registra o maior entre 2 numeros*/
+
+
+let a = 7;
+let b = 8;
+
+if (a > b) {
+console.log("a") 
+}
+else if (a < b){
+console.log("b")
+}
+
+/* EXERCICIO 3 
+Registra o maior entre 3 numeros*/
+
+let a = 9;
+let b = 8;
+let c = 10;
+
+if (a > b && a > c) {
+    console.log("a")
+} else if (b > a && b > c){
+    console.log("b")
+} else if (c > a && c > b){
+    console.log("c")
+}else {
+    console.log("são iguais")
+}
+/* EXERCICIO 4 
+programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+
+let number = 0;
+
+if (number > 0) {
+    console.log("postive")
+}
+else if (number < 0) {
+    console.log("negative")
+} else {
+    console.log("zero")
+}
+
+/* EXERCICIO 5
+ programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.*/
+
+ let angulo1 = 40;
+ let angulo2 = 100;
+ let angulo3 = 40;
+ let sum = angulo1 + angulo2 + angulo3;
+
+ if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+ console.log("error, this not is a valid angle")
+ }
+else if (sum === 180) {
+ console.log("true")
+} else {
+    console.log("false")
+}
+
+/* EXERCICIO 6
+programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+Exemplo: bishop (bispo) -> diagonals (diagonais)
+*/
+let rei = "Anda uma casa por jogada pra qualquer direção; Não passa por cima de outras peças; "
+let dama = "Anda quantas casas quiser por jogada para qualquer direção; Não passa por cima de outras peças; "
+let bispo =  "Anda quantas casas quiser por jogada e apenas na direção diagonal; Não passa por cima de outras peças;"
+let cavalo = "Anda sempre duas casas para frente e uma pro lado formando um L pra qualquer direção;  Passa por cima de outras peças; "
+let torre = "Anda quantas casas quiser por jogada apenas na direção horizontal ou vertical; Não passa por cima de outras peças; "
+let peão = "Cada peão pode andar duas casas para frente na sua primeira jogada, e uma casa pra frente nas demais;   Não passa por cima de outras peças;"
+
+
+let peça = "rei"
+
+switch (peça) {
+case "rei": 
+    console.log(rei)
+
+     break;
+
+    case "dama":    
+        console.log(dama)
+break
+case "torre":
+console.log(torre)
+break
+
+case "peão":
+    console.log(peão)
+
+    case "bispo":
+    console.log(bispo)
+
+    case "cavalo":
+
+    console.log(cavalo)
+
+    default: 
+        console.log("insira uma peça do xadrez: dama, rei, peão, torre, bispo, cavalo")
+        break;
+}
+
+//EXERCICIO 7 programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.
+    
+let notaPorcentagem = 56;
+let notaLetter; 
---
+++ Trybe exercicios/Fundamentos/JavaScript/index.js
@@ -0,0 +1,19 @@
+
+/*var base = 5;
+var altura = 8;
+var area = base * altura;
+
+console.log(area)
+*/
+
+ let resultado = 
+ function () {}
+let nota = 65;
+
+ if (nota >= 80){
+ console.log("Parabéns, você foi aprovada(o)!")
+
+}else if (nota < 80 && nota >= 60){
+    
+    resultado ='Voce esta na lista de espera' 
+}
